715beb89fba7e3419ee0cc81644368c71cb8ab5e
Taegyun Sooran Kim <T.SooranKim@gmail.com>
2021-07-14 16:56:16 +0900

    [feature/#13] 클라이언트 베이스 코드 작성했습니다. (#17)
    * devcontainer name 변경
    
    * eslint sourceType를 module로 설정
    
    * .gitignore에서 .env 제거하고 /dist 추가
    
    * 기본적인 webpack, babel, css관련 module 설정
    
    * 기본 css 파일 추가
    
    * webpack에서 image 처리할 수 있게 추가
    
    * reset.css는 .prettierignore에 추가
    
    * server, client 디렉터리를 반대로 작업한거 수정

---
+++ client/src/index.js
@@ -0,0 +1,6 @@
+import 'core-js';
+
+import '@/styles/reset.css';
+import '@/styles/global.scss';
+
+const $app = document.querySelector('#app');
---
+++ client/webpack.common.js
@@ -0,0 +1,62 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    clean: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.s?css$/,
+        exclude: /\.module\.s?css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+      {
+        test: /\.module\.s?css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/images/[hash][ext][query]',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
+};
