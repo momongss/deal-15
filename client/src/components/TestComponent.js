ee64dd8b4b8317ecbe125b9e081df6fc21bc7227
TC <37568161+momongss@users.noreply.github.com>
2021-07-15 23:06:16 +0900

    [feature/#20] 컴포넌트 베이스 코드 작성 (#23)
    * 컴포넌트 베이스 코드 작성
    
    * .vscode setting root 에 추가
    
    * update state, props -> _state, _props
    protected 로 변경
    
    * update createDom class 외 속성 지정가능하게
    
    * update script type module 삭제
    
    * update Component 기본메소드 추가
    
    * update state, props getter 삭제
    
    * update index.html script 태그 삭제
    
    * update createDom 에서 this. 직접 조작안하게 변경

---
+++ client/src/components/TestComponent.js
@@ -0,0 +1,30 @@
+import Component from '@/components/Component';
+
+export default class TestComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this._state = { test: 0 };
+
+    this.$dom = this.createDom('div', 'test-component-wrapper');
+    this.render();
+    this.addEvent();
+  }
+
+  render = () => {
+    this.$dom.innerHTML = `
+      <span class="test-component">테스트 컴포넌트 ${this._props.number}</span>
+      <span class="state-test">${this._state.test}</span>
+    `;
+  };
+
+  addEvent = () => {
+    this.$dom.addEventListener('click', (e) => {
+      if (e.target.className === 'test-component') {
+        console.log(this._props.number);
+      } else if (e.target.className === 'state-test') {
+        this.setState({ test: this._state.test + 1 });
+      }
+    });
+  };
+}
--- client/src/index.js
+++ client/src/index.js
@@ -3,4 +3,7 @@ import 'core-js';
 import '@/styles/reset.css';
 import '@/styles/global.scss';
 
-const $app = document.querySelector('#app');
+import TestPage from '@/pages/TestPage.js';
+
+const testPage = new TestPage();
+document.querySelector('#app').appendChild(testPage.$dom);
