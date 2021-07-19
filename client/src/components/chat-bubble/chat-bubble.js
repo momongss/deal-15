a118c269d86e45c813ed735734088e4de21f3870
TC <37568161+momongss@users.noreply.github.com>
2021-07-19 21:32:56 +0900

    [feature/#33] ChatBubble 컴포넌트 작성 (#63)
    * Component -> getDom 메소드 삭제
    
    * ChatBubble 컴포넌트 작성
    
    * ChatBubble 컴포넌트 테스트 코드 작성
    
    * 리뷰 내용 반영 (채팅이 끝에 닿게 하지 않게 외)
    - 채팅이 다른 쪽 끝에 닿지 않게 처리
    - margin-top 추가
    - 좌우 배치 코드 추가
    - 띄어쓰기 없는 긴 글자 대응
    - 단어로 줄바꿈 처리
    
    * 공백 문자 처리 (개행 처리는 못함.)
    
    * 깃 잘못된 것 바로잡음
    
    * Update client/src/components/chat-bubble/chat-bubble.js
    
    Co-authored-by: Taegyun Sooran Kim <T.SooranKim@gmail.com>
    
    Co-authored-by: Taegyun Sooran Kim <T.SooranKim@gmail.com>

---
+++ client/src/components/chat-bubble/chat-bubble.js
@@ -0,0 +1,33 @@
+import Component from '@/components/Component';
+
+import styles from '@/styles/components/chat-bubble/chat-bubble.module.scss';
+
+function stringPreprocess(str) {
+  return str.trim().replace(/ /g, '&nbsp;').replace(/\r?\n/gm, '<br />');
+}
+
+// props : { type, text }
+export default class ChatBubble extends Component {
+  constructor(props) {
+    super(props);
+
+    this.$dom = this.createDom('div', {
+      className: styles['chat-bubble'],
+    });
+
+    this.$dom.classList.add(styles[this._props.type]);
+
+    this.render();
+    this.addEvent();
+  }
+
+  render = () => {
+    this.$dom.innerHTML = `
+      <div class="${styles['chat-text-wrapper']}">
+        <div class="${styles['chat-text']}">${stringPreprocess(this._props.text)}</div>
+      </div>
+    `;
+  };
+
+  addEvent = () => {};
+}
---
+++ client/src/pages/chat-bubble-test-page.js
@@ -0,0 +1,40 @@
+import Component from '@/components/Component';
+
+import ChatBubble from '@/components/chat-bubble/chat-bubble.js';
+
+export default class TestPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.$dom = this.createDom('div', { className: 'test-page-wrapper' });
+
+    this.ChatBubble1 = new ChatBubble({
+      type: 'other',
+      text: '상대방의 말',
+    });
+
+    this.ChatBubble2 = new ChatBubble({
+      type: 'me',
+      text: `내가 한 말ㄴㅇㅇㅇ
+      
+      ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ            ㅇㄴㅇㅇ             ㅇㄴㅇㄴ`,
+    });
+
+    this.render();
+    this.addEvent();
+  }
+
+  render = () => {
+    this.$dom.innerHTML = `
+      <div class="ChatBubble1"></div>
+      <div class="ChatBubble2"></div>
+    `;
+
+    this.replaceElement(this.$dom.querySelector('.ChatBubble1'), this.ChatBubble1.$dom);
+    this.replaceElement(this.$dom.querySelector('.ChatBubble2'), this.ChatBubble2.$dom);
+  };
+
+  addEvent = () => {
+    //
+  };
+}
