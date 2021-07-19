d03bdb42d0bc4ccfde787dced25398cf19eb93e9
momongss <rkrp15@gmail.com>
2021-07-20 02:50:04 +0900

    DropDown 컴포넌트 작성

---
+++ client/src/components/drop-down/drop-down.js
@@ -0,0 +1,35 @@
+import Component from '@/components/Component';
+
+import styles from '@/styles/components/drop-down/drop-down.module.scss';
+
+export default class DropDown extends Component {
+  constructor(props) {
+    super(props);
+
+    this.$dom = this.createDom('div', {
+      className: styles['drop-down'],
+    });
+
+    this.render();
+    this.addEvent();
+  }
+
+  render = () => {
+    this.$dom.innerHTML = this._props.itemList
+      .map((item) => {
+        return `<div class="${styles['item']} ${styles[item.state]}">${item.label}</div>`;
+      })
+      .join('');
+
+    this.$dom.firstChild.classList.add(styles['first']);
+    this.$dom.lastChild.classList.add(styles['last']);
+  };
+
+  addEvent = () => {
+    this.$dom.querySelectorAll(`.${styles['item']}`).forEach(($item) => {
+      $item.addEventListener('click', () => {
+        this._props.onClick($item.innerHTML);
+      });
+    });
+  };
+}
